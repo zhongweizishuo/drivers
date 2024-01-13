ddf68384e05719813e3cbe287f6c55c11ac2b437
zhongweizishuo <zhongweizishuo@outlook.com>
2024-01-13 11:31:52 +0800

    gpioled

---
+++ 05_gpioled/ledApp.c
@@ -0,0 +1,61 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+/***************************************************************
+文件名		: ledApp.c
+描述	   	: 驱测试APP
+使用方法	 ：./ledApp /dev/gpioled  0 关闭LED
+		      ./ledApp /dev/gpioled  1 打开LED		
+日志	   	: 初版V1.0 2024
+***************************************************************/
+
+#define LEDOFF 	0
+#define LEDON 	1
+
+/*
+ * @description		: main主程序
+ * @param - argc 	: argv数组元素个数
+ * @param - argv 	: 具体参数
+ * @return 			: 0 成功;其他 失败
+ */
+int main(int argc, char *argv[])
+{
+	int fd, retvalue;
+	char *filename;
+	unsigned char databuf[1];
+	
+	if(argc != 3){
+		printf("Error Usage!\r\n");
+		return -1;
+	}
+
+	filename = argv[1];
+
+	/* 打开led驱动 */
+	fd = open(filename, O_RDWR);
+	if(fd < 0){
+		printf("file %s open failed!\r\n", argv[1]);
+		return -1;
+	}
+
+	databuf[0] = atoi(argv[2]);	/* 要执行的操作：打开或关闭 */
+
+	/* 向/dev/led文件写入数据 */
+	retvalue = write(fd, databuf, sizeof(databuf));
+	if(retvalue < 0){
+		printf("LED Control Failed!\r\n");
+		close(fd);
+		return -1;
+	}
+
+	retvalue = close(fd); /* 关闭文件 */
+	if(retvalue < 0){
+		printf("file %s close failed!\r\n", argv[1]);
+		return -1;
+	}
+	return 0;
+}
